5af4a6348941841bf9bb7fdc9228d0f7626420eb
v-kulikov-dev <23vitas93@mail.ru>
2020-04-20 15:20:49 +0300

    init app

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from "expo";
+import {bootstrap} from "./src/bootstrap";
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false)
+
+  if (!isReady) {
+    return (
+        <AppLoading
+          startAsync={bootstrap}
+          onFinish={() => setIsReady(true)}
+          onError={err => console.log(err)}
+        />
+    )
+  }
+
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
