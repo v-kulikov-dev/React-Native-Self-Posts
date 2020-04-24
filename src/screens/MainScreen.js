4c0b0c52c26a764b2fabdbc5a4dbc922b9224340
v-kulikov-dev <23vitas93@mail.ru>
2020-04-24 14:50:36 +0300

    added and steliasized a button to add a new post

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { DATA } from "../data";
 import { Post } from "../components/Post";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = (post) => {
@@ -21,6 +23,15 @@ export const MainScreen = ({ navigation }) => {
 
 MainScreen.navigationOptions = {
   headerTitle: "My blog",
+  headerRight: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Take photo"
+        iconName="ios-camera"
+        onPress={() => console.log("press")}
+      />
+    </HeaderButtons>
+  ),
 };
 
 const styles = StyleSheet.create({
